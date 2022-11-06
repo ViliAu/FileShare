8835c7f51308293ec7903608b6612a494a259351
ViliAu <36374215+ViliAu@users.noreply.github.com>
2022-11-06 15:59:22 +0200

    Connector works

--- project/send.c
+++ project/send.c
@@ -6,8 +6,8 @@ int main(int argc, char **argv) {
     SOCKET client = create_client_socket("127.0.0.1", "6666", 1, 1);
 
     //send_data(client, "Hello world!", (int)strlen("Hello world!"), 0);
-    send(client, "Hello world!", (int)strlen("Hello world!"), 0);
+    int iresult = send(client, "Hello world!", (int)strlen("Hello world!"), 0);
+    printf("%d", iresult);
     close_socket(client);
-    printf("ASD");
     return 0;
 }
